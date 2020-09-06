9c0d7384a70ad4b5a4789ee5e99c5178a0632d40
stackch <36052520+stackch@users.noreply.github.com>
2020-09-06 14:39:46 +0200

    Add files via upload

--- jjas/basic/for/CharImage.js
+++ jjas/basic/for/CharImage.js
@@ -1,1 +1,22 @@
+/*
+ * see https://www.simtech-ag.ch/education/internet/jjas/resources/basic/for
+ * author: Daniel Schmutz
+ * copyright: Simtech AG (https://www.simtech-ag.ch)
+*/
 
+console.log('Start CharImage ...');
+
+const xDim = 10;
+const yDim = 10;
+for(let i = 0; i < xDim; i++) {
+    let line = '';
+	for(let j = 0; j < yDim; j++) {
+        if (i == j) {
+            line += 'x';	
+        } else {
+            line += '&';								
+        }
+    }
+    console.log(line);
+}
+console.log('... CharImage Done');
