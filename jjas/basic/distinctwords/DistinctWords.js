da154dd4f9f9882d991ea696032561f0bf82305d
stackch <36052520+stackch@users.noreply.github.com>
2020-09-06 14:43:11 +0200

    Add files via upload

--- jjas/basic/distinctwords/DistinctWords.js
+++ jjas/basic/distinctwords/DistinctWords.js
@@ -1,1 +1,23 @@
+/*
+ * see https://www.simtech-ag.ch/education/internet/jjas/resources/basic/distinctwords
+ * author: Daniel Schmutz
+ * copyright: Simtech AG (https://www.simtech-ag.ch)
+*/
 
+console.log('Start Distinct Words ...');
+
+let sentence = "if it is to be it is up to me to delegate";
+let words = sentence.split(" ");
+let distinctWords = new Set();
+
+for (let i = 0, len = words.length; i < len; i++) {
+    let word = words[i];
+    if (distinctWords.has(word)) {
+        console.log('distinct word ', word, ' detected');
+    }
+    distinctWords.add(word);
+}
+
+console.log('distinctWords = ', distinctWords);
+
+console.log('... Done Distinct Words');
