0158381a90928bb3aae2fa8c20fcf253bc68eebd
stackch <36052520+stackch@users.noreply.github.com>
2020-09-06 14:43:54 +0200

    Add files via upload

--- jjas/meteo/classic/MeteoClassic.js
+++ jjas/meteo/classic/MeteoClassic.js
@@ -1,1 +1,51 @@
+console.log('Start...');
 
+function MeteoClassic() {
+    let ta = [ 19, 25, 28, 29, 24, 21, 20, 18, 23 ]; // Quelle Google
+    // compute min, max, mittelwert
+    let min = ta[0];
+    let max = ta[0];
+    let med = 0;
+    for (let i = 0; i < ta.length; i++) {
+        if (min > ta[i]) {
+            min = ta[i];
+        }
+        if (max < ta[i]) {
+            max = ta[i];
+        }
+        med += ta[i];
+    }
+    med /= ta.length;
+    console.log("min = ", min);
+    console.log("max = ", max);
+    console.log("med = ", med.toFixed(2), "\n");
+    if (max >= 35) {
+        console.log("Wüstentag");
+    }
+    if (max >= 30 && max < 35) {
+        console.log("Heisser Tag");
+    }
+    if (max >= 25 && max < 30) {
+        console.log("Sommertag");
+    }
+    if (min >= 20) {
+        console.log("Tropennacht");
+    }
+    if (med < 12) {
+        console.log("Heiztag");
+    }
+    if (med >= 5) {
+        console.log("Vegetationstag");
+    }
+    if (min < 0 && max >= 0) {
+        console.log("Frosttag");
+    }
+    if (max < 0) {
+        console.log("Eistag");
+    }
+}
+
+// run meteo classic method
+MeteoClassic()
+
+console.log('...Done');
