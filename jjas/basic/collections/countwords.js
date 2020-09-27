31e78edf0380fd0ea74d0417eb83a1cc933c5ab0
Daniel Schmutz <ds@simtech-ag.ch>
2020-09-27 12:21:27 +0200

    jjas exercises basic

---
+++ jjas/basic/collections/countwords.js
@@ -0,0 +1,15 @@
+console.log('Start Count Words ...');
+
+let sentence = "if it is to be it is up to me to delegate";
+let words = sentence.split(" ");
+let wordCounter = new Map();
+
+for (let i = 0, len = words.length; i < len; i++) {
+    let word = words[i];
+    let freq = wordCounter.get(word);
+    wordCounter.set(word, (!freq ? 1 : freq + 1));
+}
+
+console.log('wordCounter = ', wordCounter);
+
+console.log('... Done Count Words');
---
+++ jjas/basic/collections/distinctwords.js
@@ -0,0 +1,17 @@
+console.log('Start Distinct Words ...');
+
+let sentence = "if it is to be it is up to me to delegate";
+let words = sentence.split(" ");
+let distinctWords = new Set();
+
+for (let i = 0, len = words.length; i < len; i++) {
+    let word = words[i];
+    if (distinctWords.has(word)) {
+        console.log('distinct word ', word, ' detected');
+    }
+    distinctWords.add(word);
+}
+
+console.log('distinctWords = ', distinctWords);
+
+console.log('... Done Distinct Words');
