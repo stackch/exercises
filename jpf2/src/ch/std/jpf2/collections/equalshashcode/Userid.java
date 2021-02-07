b7e13d0723e25477e17a361814f6c2ddc6e4ec46
simtech <ds@simtech-ag.ch>
2021-02-07 16:42:03 +0100

    collections equals and hashcode

---
+++ jpf2/src/ch/std/jpf2/collections/equalshashcode/Userid.java
@@ -0,0 +1,55 @@
+package ch.std.jpf2.collections.equalshashcode;
+
+public class Userid {
+
+	private String userid;
+	private String password;
+
+	public Userid() {
+		this("", "");
+	}
+
+	public Userid(String userid) {
+		this(userid, "");
+	}
+
+	public Userid(String userid, String password) {
+		super();
+		this.password = password;
+		this.userid = userid;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (obj instanceof Userid) {
+			Userid userid = (Userid)obj;
+			if (!this.userid.equals(userid.userid)) {
+				return false;
+			}
+			if (!this.password.equals(userid.password)) {
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		int hashCode = this.userid.hashCode();
+		hashCode += 31 * this.password.hashCode();
+		return hashCode;
+	}
+	
+	@Override
+	public String toString() {
+		return "userid:" + userid + ", password:" + password;
+	}
+
+}
---
+++ jpf2/src/ch/std/jpf2/collections/equalshashcode/UseridTestCase.java
@@ -0,0 +1,62 @@
+package ch.std.jpf2.collections.equalshashcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class UseridTestCase {
+
+	@Test
+	public void testSameEquals() {
+		Userid user1 = new Userid("hans", "123");
+		Userid user2 = user1;
+
+		Assert.assertSame(user1, user2);
+		Assert.assertEquals(user1, user2);
+	}
+
+	@Test
+	public void testNotSame() {
+		Userid user1 = new Userid("hans", "123");
+		Userid user2 = new Userid("peter", "456");
+
+		Assert.assertNotSame(user1, user2);
+	}
+
+	@Test
+	public void testEquals() {
+		Userid user1 = new Userid("hans", "123");
+		Userid user2 = new Userid("hans", "123");
+
+		Assert.assertEquals(user1, user2);
+	}
+
+	@Test
+	public void testHashcode() {
+		Userid user1 = new Userid("hans", "123");
+		Userid user2 = new Userid("hans", "123");
+
+		Assert.assertEquals(user1.hashCode(), user2.hashCode());
+	}
+
+	@Test
+	public void testSet() {
+		Userid user1 = new Userid("hans", "123");
+		Userid user2 = new Userid("hans", "123");
+		Userid user3 = new Userid("peter", "456");
+		Userid user4 = new Userid("peter", "456");
+		
+		Set<Userid> useridSet = new HashSet<Userid>();
+		useridSet.add(user1);
+		useridSet.add(user2);
+		useridSet.add(user3);
+		useridSet.add(user4);
+		
+		Assert.assertEquals(2, useridSet.size());
+		
+
+	}
+
+}
